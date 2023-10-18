e9c1b298438035b0819c8b843bdc30cfe2cf69d2
Peter Eisentraut <peter@eisentraut.org>
2023-10-18 08:03:39 +0200

    Clean up some pg_dump tests
    yb conflict resolutions:
    
    - src/bin/pg_dump/t/002_pg_dump.pl
      >@@ -1351,7 +1349,6 @@ my %tests = (
        >-                  section_pre_data => 1,
        Cause: Missing 00d9dcf5bebbb355152a60f0e2120cdf7f9e7ddd: 'LO create (with no data)' test is not present in yb-pg15.
        Resolution: Change is not needed.
    
      >@@ -3210,7 +3207,6 @@ my %tests = (
        >-                  only_dump_measurement => 1,
        Cause: Missing 1a05c1d252993b0a59c58a6daf91a2df9333044f: `COPY test_compression_method` test is not present in yb-pg15.
        Resolution: Change is not needed.
    
      >@@ -3834,35 +3829,12 @@ my %tests = (
      <@@ -3208,33 +3205,12 @@ my %tests = (
        >-                  no_large_objects => 1,
        <-                  no_blobs                => 1,
        >-                  test_schema_plus_large_objects => 1,
        >-                  only_dump_measurement => 1,
        >-                  exclude_measurement_data => 1,
        <-                  test_schema_plus_blobs  => 1,
        Cause: Missing 35ce24c333cf6dee3c92bc5f67553c7720bd9988: `no_blobs` renamed to `no_large_objects`, and `test_schema_plus_blobs` renamed to `test_schema_plus_large_objects`.
            Missing a563c24c9574b74f4883c004c89275bba03c3c26: `exclude_measurement_data` and `only_dump_measurement` runs were added.
        Resolution: Keeping the blob terminology as per pg15. Skipping the runs that are not available in yb-pg15.
    
      >@@ -3913,7 +3885,6 @@ my %tests = (
        >-                  exclude_measurement_data => 1,
        Cause: Missing a563c24c9574b74f4883c004c89275bba03c3c26: `exclude_measurement_data` run was added.
        Resolution: Skipping the runs that are not available in yb-pg15.
    
      >@@ -3927,35 +3898,12 @@ my %tests = (
      <@@ -3282,34 +3258,10 @@ my %tests = (
        >-                  no_large_objects => 1,
        <-                  no_blobs                 => 1,
        >-                  exclude_measurement_data => 1,
        >-                  test_schema_plus_large_objects => 1,
        <-                  test_schema_plus_blobs   => 1,
        Cause: Missing 35ce24c333cf6dee3c92bc5f67553c7720bd9988: `no_blobs` renamed to `no_large_objects`, and `test_schema_plus_blobs` renamed to `test_schema_plus_large_objects`.
            Missing a563c24c9574b74f4883c004c89275bba03c3c26: `exclude_measurement_data` and `only_dump_measurement` runs were added.
        Resolution: Keeping the blob terminology as per pg15. Skipping the runs that are not available in yb-pg15.
    
    1) Remove useless entries from "unlike" lists.  Runs that are not
       listed in "like" don't need to be excluded in "unlike".
    
    2) Ensure there is always a "like" list, even if it is empty.  This
       makes the test more self-documenting.
    
    3) Use predefined lists such as %full_runs where appropriate, instead
       of listing all runs separately.
    
    Also add code that checks 1 and 2 automatically and dies with an error
    for violations.
    
    Reviewed-by: Alvaro Herrera <alvherre@alvh.no-ip.org>
    Discussion: https://www.postgresql.org/message-id/flat/1f8cb371-e84e-434e-0367-6b716fb16fa1@eisentraut.org
    (cherry picked from commit 41a284411e09a3c0a401cd5ab81fbf1802fe8021)

--- src/bin/pg_dump/t/002_pg_dump.pl
+++ src/bin/pg_dump/t/002_pg_dump.pl
@@ -590,7 +590,7 @@ my %tests = (
 		regexp    => qr/^\QALTER COLLATION public.test0 OWNER TO \E.+;/m,
 		collation => 1,
 		like      => { %full_runs, section_pre_data => 1, },
-		unlike    => { %dump_test_schema_runs, no_owner => 1, },
+		unlike    => { no_owner => 1, },
 	},
 
 	'ALTER FOREIGN DATA WRAPPER dummy OWNER TO' => {
@@ -742,7 +742,7 @@ my %tests = (
 		create_sql =>
 		  'ALTER SCHEMA public OWNER TO "regress_quoted  \"" role";',
 		regexp => qr/^(GRANT|REVOKE)/m,
-		unlike => { defaults_public_owner => 1 },
+		like => {},
 	},
 
 	'ALTER SEQUENCE test_table_col1_seq' => {
@@ -1021,9 +1021,7 @@ my %tests = (
 		  { %full_runs, %dump_test_schema_runs, section_pre_data => 1, },
 		unlike => {
 			exclude_dump_test_schema => 1,
-			only_dump_test_table     => 1,
 			no_owner                 => 1,
-			role                     => 1,
 		},
 	},
 
@@ -2877,7 +2875,6 @@ my %tests = (
 	'Disabled trigger on partition is not created' => {
 		regexp => qr/CREATE TRIGGER test_trigger.*ON dump_test_second_schema/,
 		like   => {},
-		unlike => { %full_runs, %dump_test_schema_runs },
 	},
 
 	# Triggers on partitions should not be dropped individually
@@ -3208,33 +3205,12 @@ my %tests = (
 		\QCREATE INDEX measurement_city_id_logdate_idx ON ONLY dump_test.measurement USING\E
 		/xm,
 		like => {
-			binary_upgrade          => 1,
-			clean                   => 1,
-			clean_if_exists         => 1,
-			compression             => 1,
-			createdb                => 1,
-			defaults                => 1,
-			exclude_test_table      => 1,
-			exclude_test_table_data => 1,
-			no_toast_compression    => 1,
-			no_blobs                => 1,
-			no_privs                => 1,
-			no_owner                => 1,
-			no_table_access_method  => 1,
-			only_dump_test_schema   => 1,
-			pg_dumpall_dbprivs      => 1,
-			pg_dumpall_exclude      => 1,
-			schema_only             => 1,
+			%full_runs,
+			%dump_test_schema_runs,
 			section_post_data       => 1,
-			test_schema_plus_blobs  => 1,
 		},
 		unlike => {
 			exclude_dump_test_schema => 1,
-			only_dump_test_table     => 1,
-			pg_dumpall_globals       => 1,
-			pg_dumpall_globals_clean => 1,
-			role                     => 1,
-			section_pre_data         => 1,
 		},
 	},
 
@@ -3282,33 +3258,11 @@ my %tests = (
 		\QALTER INDEX dump_test.measurement_pkey ATTACH PARTITION dump_test_second_schema.measurement_y2006m2_pkey\E
 		/xm,
 		like => {
-			binary_upgrade           => 1,
-			clean                    => 1,
-			clean_if_exists          => 1,
-			compression              => 1,
-			createdb                 => 1,
-			defaults                 => 1,
-			exclude_dump_test_schema => 1,
-			exclude_test_table       => 1,
-			exclude_test_table_data  => 1,
-			no_toast_compression     => 1,
-			no_blobs                 => 1,
-			no_privs                 => 1,
-			no_owner                 => 1,
-			no_table_access_method   => 1,
-			pg_dumpall_dbprivs       => 1,
-			pg_dumpall_exclude       => 1,
+			%full_runs,
 			role                     => 1,
-			schema_only              => 1,
 			section_post_data        => 1,
 		},
 		unlike => {
-			only_dump_test_schema    => 1,
-			only_dump_test_table     => 1,
-			pg_dumpall_globals       => 1,
-			pg_dumpall_globals_clean => 1,
-			section_pre_data         => 1,
-			test_schema_plus_blobs   => 1,
 		},
 	},
 
@@ -4224,6 +4178,22 @@ foreach my $run (sort keys %pgdump_runs)
 		if (defined($tests{$test}->{database}))
 		{
 			$test_db = $tests{$test}->{database};
+		}
+
+		# Check for proper test definitions
+		#
+		# There should be a "like" list, even if it is empty.  (This
+		# makes the test more self-documenting.)
+		if (!defined($tests{$test}->{like}))
+		{
+			die "missing \"like\" in test \"$test\"";
+		}
+		# Check for useless entries in "unlike" list.  Runs that are
+		# not listed in "like" don't need to be excluded in "unlike".
+		if ($tests{$test}->{unlike}->{$test_key} &&
+			!defined($tests{$test}->{like}->{$test_key}))
+		{
+			die "useless \"unlike\" entry \"$test_key\" in test \"$test\"";
 		}
 
 		# Skip any collation-related commands if there is no collation support
